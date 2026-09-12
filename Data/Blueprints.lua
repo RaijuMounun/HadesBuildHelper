d908f738821414bfab2392cfd29a9db14df3b224
Eren Keskinoğlu <87018234+RaijuMounun@users.noreply.github.com>
2026-09-12 19:50:36 +0300

    Create Blueprints.lua with example meta-builds

---
+++ Data/Blueprints.lua
@@ -0,0 +1,16 @@
+local Blueprints = {
+    {
+        Name = "Sea-Storm Rail",
+        CoreTraits = { "PoseidonWeaponBoon", "ZeusWeaponBoon", "SeaStormDuo" },
+        Gods = { "Poseidon", "Zeus" },
+        Weapon = "Rail"
+    },
+    {
+        Name = "Merciful End Fists",
+        CoreTraits = { "AthenaWeaponBoon", "AresWeaponBoon", "MercifulEndDuo" },
+        Gods = { "Athena", "Ares" },
+        Weapon = "Fists"
+    }
+}
+
+return Blueprints
