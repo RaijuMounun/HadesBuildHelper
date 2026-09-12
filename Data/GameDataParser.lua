d14184402c1deb15910702b376246a2c50c58619
Eren Keskinoğlu <87018234+RaijuMounun@users.noreply.github.com>
2026-09-12 19:21:34 +0300

    Create GameDataParser to parse trait data

---
+++ Data/GameDataParser.lua
@@ -0,0 +1,19 @@
+-- GameDataParser.lua
+local GameDataParser = {}
+
+function GameDataParser.ParseTraits(traitData)
+    local parsed = {}
+    if not traitData then return parsed end
+    
+    for k, v in pairs(traitData) do
+        parsed[k] = {
+            Name = v.Name or k,
+            God = v.God or "None",
+            Slot = v.Slot or "None"
+        }
+    end
+    
+    return parsed
+end
+
+return GameDataParser
